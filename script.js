1f9008b4c6560845c1a8d71e71201802291a2aaf
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-01-04 14:23:27 +0000

    Planning

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(characterAmount, includeUppercase, includeSymbols, includeNumbers) {
+  console.log("Button has been clicked")
+  prompt ("Would you like your password to have uppercase letters? Please write Yes or No.")
+  if ("yes")
+  var includeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var includeLowercase = "abcdefghijklmnopqrstuvwxyz"
+  var includeNumbers = "0123456789"
+  //prompt the user
+  //password legnth 8 < 128
+  //lowercase, uppercase, special characters
+  //validate input
+  //generate password
+  //display password
+  return "Generated Password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
