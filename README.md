1fcf1f094337df601238d951c13eeabfcbacf499
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-01-07 15:50:16 +0000

    adding comments and creating an order

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ## Declaring the Variables
 
 
+
 ##  
 
 var is function scoped and let is block scoped. 
+
+
+The general idea : 
+
+The variables will be declared at the start. So the possibilities are a set of uppercase, lowercase, special characters. The length could be put in the first order of appearance. The var userSpecification = '' will act as a sort of 'empty basket' the user will then work through and pick the desired 'contents' by answering the questions provided eg include uppercase? The 'Contents' will be added to 'empty basket' to build the criteria for the password. The password will then be created at random based on the criteria the user has selected.
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Generates the button which will then kickstart the process of events
+generateBtn.addEventListener("click", writePassword);
 
 //Setting the variables
 function generatePassword(){
@@ -11,20 +13,6 @@ function generatePassword(){
   var password = '';
   var userSpecification = '';
 
-// What is the length of the password?
-var plength = prompt('Enter your password length (must be a number between 8 and 128).')
-
-// Is the password long enough?
-if (plength > 8 && plength < 128) {
-  console.log(plength + " characters in length")
-}
-
-// if not, alert the user to try again.
-else {
-  alert('Must be between 8 - 128 characters.')
-  return '';
-}
-
 // Questions
 
 //These will ask the user for the specifications of their password.
@@ -34,15 +22,13 @@ var uppercaseQuestion = confirm('Include uppercase letters?')
 var numbersQuestion = confirm('Include numbers?')
 var specialQuestion = confirm('Include special characters?')
 
-
 //Conditions
 
-//These will determine the outcome of the password based on the user's answers to the questions.
+//These will determine the outcome of the password based on the user's answers to the questions.  
 
 if (lowercaseQuestion) {
   userSpecification += lowercase
   console.log ("lowercase chosen")
-  
 }
 if (uppercaseQuestion) {
   userSpecification += uppercase
@@ -57,13 +43,25 @@ if (specialQuestion) {
   console.log ("special chosen")
 }
 
-
 // If these conditions are NOT met, the user will be asked to retry.
 if (
   !uppercaseQuestion && !lowercaseQuestion && !numbersQuestion && !specialQuestion
 ) {
   return alert('You need at least 1: Uppercase, Numbers or Special Characters');
-  
+}
+
+// What is the length of the password?
+var plength = prompt('Enter your password length (must be a number between 8 and 128).')
+
+// Is the password long enough?
+if (plength > 7 && plength < 129) {
+  console.log(plength + " characters in length")
+}
+
+// if not, alert the user to try again.
+else {
+  alert('Must be between 8 - 128 characters.')
+  return '';
 }
 
 //Randomly generates the password based on criteria selected by the user.
@@ -74,19 +72,17 @@ for (let i = 0; i < plength; i= i + 1) {
 return password;
 }
 
-
+//Getting the password to appear on the screen
 function writePassword() {
-  var password = generatePassword();
+var password = generatePassword();
 
-  //Matches the id declared in the CSS so will be displayed on page
-  var actualPassword = document.querySelector("#password");
-  actualPassword.value = password;
-  console.log ("generated password = " + password)
-
+//Matches the id declared in the CSS and html so will be displayed in the middle of the page
+var actualPassword = document.querySelector("#password");
+actualPassword.value = password;
+console.log ("generated password = " + password)
 }
 
-// Generates the button
-generateBtn.addEventListener("click", writePassword);
+
 
   
 
